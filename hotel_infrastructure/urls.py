30da0f95e38bb9ac79760299d418158c730370cf
Spaceginner <ivan.demian2009@gmail.com>
2025-07-23 11:47:33 +0300

    include roomcategories & roomfeatures

--- hotel_infrastructure/urls.py
+++ hotel_infrastructure/urls.py
@@ -1,10 +1,12 @@
 from rest_framework.routers import SimpleRouter
 
-from .views import BuildingViewSet, FloorViewSet
+from .views import BuildingViewSet, FloorViewSet, RoomCategoryViewSet, RoomFeatureViewSet
 
 router = SimpleRouter()
 router.register('buildings', BuildingViewSet, basename='building')
 router.register('floors', FloorViewSet, basename='floor')
+router.register('roomcategories', RoomCategoryViewSet)
+router.register('roomfeatures', RoomFeatureViewSet)
 
 app_name = "hotel_infrastructure"
 urlpatterns = router.urls
